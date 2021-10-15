870f5ee81c494e90f1b5664f86d626d01a4dc984
Andrii <droid85@yahoo.com>
2021-10-15 16:19:50 +0300

    corrected html file. add constructor function in js file

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+class Tabs() {
+    this.btnTag = 'buttons';
+    this.contentTags = 'div';
+    this.btnNames = ['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4'];
+    this.blockContents = ['Content 1', 'Content 2', 'Content 3', 'Content 4'];
+}
+
